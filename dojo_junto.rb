9b91f62a0192df26ff0427801797aec149328923
Raphael Molesim <raphael.molesim@gmail.com>
2018-12-18 19:39:57 -0200

    WIP

--- dojo_junto.rb
+++ dojo_junto.rb
@@ -3,13 +3,31 @@ class DojoJunto
   def soma_romanos param1, param2
     if param2 == "III"
       param2 = "V"
-    end 
+    end
     
     if param1 == "III"
       param1 = "V"
     end 
     
+    if param1+param2=="IVI"
+      return "V"
+    end 
+    # param1_d = param1.split.collect{|p| biblioteca p}
+    # param2_d = param2.split.collect{|p| biblioteca p}
+    
     [param1, param2].sort.join
+  end
+  
+  def biblioteca caracter
+    if caracter == "I"
+      1
+    end
+    if caracter == "V"
+      5
+    end
+    if caracter == "X"
+      10
+    end
   end
   
 end
